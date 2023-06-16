d5fd111e46d812d0e28627a1de1afa00e03e9c18
engNesta <robert.nuhu0002@stud.hkr.se>
2023-06-16 06:18:43 +0200

    Gui Setup set

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::Component, public juce::Button::Listener
 {
 public:
     //==============================================================================
@@ -15,13 +15,20 @@ public:
     ~MainComponent() override;
 
     //==============================================================================
+    juce::TextButton loadButton;
+    juce::Label infoLabel;
+
+    //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void buttonClicked(juce::Button *button) override;
+    void loadFile();
+    void retrieveVST3data(juce::File &file);
 
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    std::unique_ptr<juce::FileChooser> fileChooser;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
